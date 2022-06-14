b1b07da283470c85cab8933a2bd8e2cc5a488a44
Daniel Sodré <danielcsodre98@gmail.com>
2022-06-14 17:20:24 -0300

    declaração das variáveis

--- src/main.java
+++ src/main.java
@@ -2,5 +2,9 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        int n1 = 0;
+        int n2 = 0;
+        n1 = input.nextInt();
+        n2 = input.nextInt();
     }
 }
