877bf21d7d9e3b9a8f4e79c0074390d2c35a37cc
Daniel Sodré <danielcsodre98@gmail.com>
2022-06-14 18:20:58 -0300

    criacao opcao 3 e 4

--- src/main.java
+++ src/main.java
@@ -6,6 +6,7 @@ public class main {
         int n1;
         int n2;
         int opcao;
+        String desejaRepetir;
 
         System.out.println("CALCULADORA BÁSICA");
         System.out.println("1- SOMA");
@@ -26,7 +27,7 @@ public class main {
                 int nf = n1 + n2;
                 System.out.println("A soma de " + n1 + " + " + n2 + " é = " + nf);
 
-            } else if(opcao == 2) {
+            } else if (opcao == 2) {
                 System.out.println("Você escolheu SUBTRAÇÃO");
                 System.out.print("Digite o primeiro número da subtração: ");
                 n1 = input.nextInt();
@@ -36,11 +37,27 @@ public class main {
                 System.out.println("A subtração de " + n1 + " - " + n2 + " é = " + nf);
 
             } else if (opcao == 3) {
+                System.out.println("Você escolheu MULTIPLICAÇÃO");
+                System.out.print("Digite o primeiro número da multiplicação: ");
+                n1 = input.nextInt();
+                System.out.print("Digite o segundo número da multiplicação: ");
+                n2 = input.nextInt();
+                int nf = n1 * n2;
+                System.out.println("A multiplicação de " + n1 + " X " + n2 + " é = " + nf);
 
-
+            } else if (opcao == 4) {
+                System.out.println("Você escolheu DIVISÃO");
+                System.out.print("Digite o primeiro número da divisão: ");
+                n1 = input.nextInt();
+                System.out.print("Digite o segundo número da divisão: ");
+                n2 = input.nextInt();
+                int nf = n1 / n2;
+                System.out.println("A divisão de " + n1 + " / " + n2 + " é = " + nf);
             }
+
         break;
         }
+
 
     }
 }
